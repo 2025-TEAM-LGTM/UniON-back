48b83de61acb29443345c65b38576d61d7f33a97
jade <syeong8663@gmail.com>
2026-02-22 02:43:26 +0900

    feat(#49): s3와 백엔드 연결

--- demo/src/main/java/com/union/demo/config/AwsConfig.java
+++
@@ -1,4 +0,0 @@
-package com.union.demo.config;
-//AWS 연결 관련 파일
-public class AwsConfig {
-}
--- demo/src/main/java/com/union/demo/utill/S3Uploader.java
+++ demo/src/main/java/com/union/demo/utill/S3Uploader.java
@@ -1,5 +1,43 @@
 package com.union.demo.utill;
-//이미지를 s3로 올리는 파일
-//s3와 통신하는 파일
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.model.PutObjectAclRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class S3Uploader {
+    private final S3Presigner presigner;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String createPresignedUrl(String contentType){
+        String key="uploads/"+ UUID.randomUUID();
+
+        PutObjectRequest  putObjectRequest= PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .putObjectRequest(putObjectRequest)
+                .build();
+
+        return presigner
+                .presignPutObject(presignRequest)
+                .url().toString();
+
+    }
+
+
 }
